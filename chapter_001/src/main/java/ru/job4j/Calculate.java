78de86c30dc0ab12b4d34c1f4f70e5d52d00d091
Roman Baranov <baranov.rp@gmail.com>
2017-12-31 21:55:22 +0100

    1.3. JavaDoc. Code convention[#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,32 @@
 package ru.job4j;
 
+/**
+ * This class prints text messages to console
+ *
+ * @author baranovr
+ * @version 1
+ */
+
 public class Calculate {
-	public static void main(String[] args) {
+	/**
+     * Print text message to console
+     * @param args the args to output
+     */
+	public static void main(final String[] args) {
 		System.out.println("Hello world!");
+	}
+
+    /**
+     * Triples the text message
+     * @param value the value for tripling
+     * @return formatted text message
+     */
+	public final String echo(final String value) {
+		if (value != null) {
+			value = String.format("%s value = ", value);
+		} else {
+			value = "value = null";
+		}
+		return String.format("%s %s %s", value, value, value);
 	}
 }
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Roman Baranov (baranov.r.p@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
