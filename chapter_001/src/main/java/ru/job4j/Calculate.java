817b87a3fae271709dc51c4186489de2358fe61c
Roman Baranov <baranov.rp@gmail.com>
2018-01-01 16:46:25 +0100

    4. Использование Git в IDEA [#31170]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -8,16 +8,18 @@ package ru.job4j;
  */
 
 public class Calculate {
-	/**
+    /**
      * Print text message to console
+     *
      * @param args the args to output
      */
-	public static void main(final String[] args) {
-		System.out.println("Hello world!");
-	}
+    public static void main(final String[] args) {
+        System.out.println("Hello world!");
+    }
 
     /**
      * Method echo.
+     *
      * @param name Your name.
      * @return Echo plus your name.
      */
