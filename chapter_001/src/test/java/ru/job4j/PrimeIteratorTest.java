beafaf4c3b87d338371ab5e93a467543da6c348f
Roman Baranov <baranov.rp@gmail.com>
2018-01-02 13:59:09 +0100

    5.1.3. Создать итератор простые числа. [#151]

---
+++ chapter_001/src/test/java/ru/job4j/PrimeIteratorTest.java
@@ -0,0 +1,66 @@
+package ru.job4j;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Test.
+ *
+ * @author Roman Baranov (baranov.rp@gmail.com)
+ * @version 1
+ */
+public class PrimeIteratorTest {
+
+    private Iterator it;
+
+    @Before
+    public void setUp() {
+        it = new PrimeIterator(new int[]{1, 2, 3, 4, 5, 6, 7, 3571});
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void shouldReturnPrimeNumbersOnly() {
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(3));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(5));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(7));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(3571));
+        assertThat(it.hasNext(), is(false));
+        it.next();
+    }
+
+    @Test
+    public void sequentialHasNextInvocationDoesntAffectRetrievalOrder() {
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(5));
+        assertThat(it.next(), is(7));
+        assertThat(it.next(), is(3571));
+    }
+
+    @Test
+    public void shouldReturnFalseCauseThereIsNoAnyPrimeNumber() {
+        it = new PrimeIterator(new int[]{4, 6});
+        assertThat("should return false, cause there is no any prime number",
+            it.hasNext(), is(false));
+    }
+
+    @Test
+    public void arrayWithNegativeNumbers() {
+        it = new PrimeIterator(new int[]{-4, -6});
+        assertThat("should return false, cause there is no any prime number",
+            it.hasNext(), is(false));
+    }
+}
