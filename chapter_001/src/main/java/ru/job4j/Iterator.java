6604f88292914fea402c70825e82088a31928f61
Roman Baranov <baranov.rp@gmail.com>
2018-01-06 17:45:10 +0100

    5.1.1. Итератор для двухмерного массива int[][] [ #9539]

---
+++ chapter_001/src/main/java/ru/job4j/Iterator.java
@@ -0,0 +1,27 @@
+package ru.job4j;
+
+import java.util.NoSuchElementException;
+
+/**
+ * An iterator over a collection.
+ *
+ * @author Roman Baranov (baranov.rp@gmail.com)
+ * @version 1
+ */
+public interface Iterator {
+
+    /**
+     * Returns the next element in the iteration.
+     *
+     * @return the next element in the iteration
+     * @throws NoSuchElementException if the iteration has no more elements
+     */
+    Object next();
+
+    /**
+     * Returns {@code true} if the iteration has more elements.
+     *
+     * @return {@code true} if the iteration has more elements
+     */
+    boolean hasNext();
+}
