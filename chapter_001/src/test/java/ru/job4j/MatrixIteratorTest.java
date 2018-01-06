dc12dd040844398e736be89a0484e08f45e4d005
Roman Baranov <baranov.rp@gmail.com>
2018-01-06 17:21:48 +0100

    5.1.1. Итератор для двухмерного массива int[][][#35756] Fix hasNext method

--- chapter_001/src/test/java/ru/job4j/MatrixIteratorTest.java
+++ chapter_001/src/test/java/ru/job4j/MatrixIteratorTest.java
@@ -20,12 +20,19 @@ public class MatrixIteratorTest {
     @Test(expected = NoSuchElementException.class)
     public void squareArrayHasNextNextSequentialInvocation() {
         it = new MatrixIterator(new int[][]{{1, 2, 3}, {4, 5, 6}});
+        assertThat(it.hasNext(), is(true));
         assertThat(it.next(), is(1));
+        assertThat(it.hasNext(), is(true));
         assertThat(it.next(), is(2));
+        assertThat(it.hasNext(), is(true));
         assertThat(it.next(), is(3));
+        assertThat(it.hasNext(), is(true));
         assertThat(it.next(), is(4));
+        assertThat(it.hasNext(), is(true));
         assertThat(it.next(), is(5));
+        assertThat(it.hasNext(), is(true));
         assertThat(it.next(), is(6));
+        assertThat(it.hasNext(), is(false));
         it.next();
     }
 
@@ -44,6 +51,8 @@ public class MatrixIteratorTest {
     @Test(expected = NoSuchElementException.class)
     public void squareArrayHasNextSequentialInvocationDoesntAffectRetrievalOrder() {
         it = new MatrixIterator(new int[][]{{1, 2, 3}, {4, 5, 6}});
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
         assertThat(it.next(), is(1));
         assertThat(it.next(), is(2));
         assertThat(it.next(), is(3));
@@ -65,6 +74,8 @@ public class MatrixIteratorTest {
     @Test(expected = NoSuchElementException.class)
     public void jaggedArrayHasNextSequentialInvocationDoesntAffectRetrievalOrder() {
         it = new MatrixIterator(new int[][]{{1}, {3, 4}});
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
         assertThat(it.next(), is(1));
         assertThat(it.next(), is(3));
         assertThat(it.next(), is(4));
@@ -74,9 +85,13 @@ public class MatrixIteratorTest {
     @Test(expected = NoSuchElementException.class)
     public void jaggedArrayHasNextNextSequentialInvocation() {
         it = new MatrixIterator(new int[][]{{1}, {3, 4}});
+        assertThat(it.hasNext(), is(true));
         assertThat(it.next(), is(1));
+        assertThat(it.hasNext(), is(true));
         assertThat(it.next(), is(3));
+        assertThat(it.hasNext(), is(true));
         assertThat(it.next(), is(4));
+        assertThat(it.hasNext(), is(false));
         it.next();
     }
 }
