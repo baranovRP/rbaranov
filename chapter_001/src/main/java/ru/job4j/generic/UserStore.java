cfa2238b5160b5490ae4f437cf2d5cf5f03913f2
baranovrp <baranov.r.p@gmail.com>
2018-01-11 17:40:50 +0100

    JOB4J-157 5.2.2. Реализовать Store<T extends Base> [ #157]

---
+++ chapter_001/src/main/java/ru/job4j/generic/UserStore.java
@@ -0,0 +1,12 @@
+package ru.job4j.generic;
+
+/**
+ * @author Roman Baranov (baranov.rp@gmail.com)
+ * @version 1
+ */
+public class UserStore extends AbstractStore<User> {
+
+    public UserStore(int size) {
+        super(size);
+    }
+}
