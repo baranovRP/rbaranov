14502f33ab8213b8afee6b6bd04f1f7562bcbd7d
baranovrp <baranov.r.p@gmail.com>
2018-01-11 17:59:18 +0100

    JOB4J-996 Resolve merge conflicts

---
+++ chapter_001/src/main/java/ru/job4j/generic/Store.java
@@ -0,0 +1,16 @@
+package ru.job4j.generic;
+
+/**
+ * @author Roman Baranov (baranov.rp@gmail.com)
+ * @version 1
+ */
+public interface Store<T extends Base> {
+
+    void add(T model);
+
+    boolean replace(String id, T model);
+
+    boolean delete(String id);
+
+    T findById(String id);
+}
