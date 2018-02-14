6ae75c3328c4470a57d836d1479936b07be9c453
romanbaranov <baranov.rp@gmail.com>
2018-02-14 22:16:41 +0100

    JOB4J-1099 Refactor threadpool constructor
    Use poll with timeout
    Add synchronized sections

--- chapter_002/src/main/java/ru/job4j/waitnotify/threadpool/Work.java
+++ chapter_002/src/main/java/ru/job4j/waitnotify/threadpool/Work.java
@@ -1,7 +1,5 @@
 package ru.job4j.waitnotify.threadpool;
 
-import static java.lang.Thread.sleep;
-
 /**
  * Work.
  *
@@ -25,11 +23,6 @@ public class Work implements Runnable {
         System.out.printf("start: %s%n", name);
         for (int i = 0; i < 10; i++) {
             System.out.printf("%s: %d%n", name, i);
-            try {
-                sleep(1L);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
         }
         System.out.printf("finish: %s%n", name);
     }
