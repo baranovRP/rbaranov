ac9a37e953e76285d884995c2bcf0a188be84665
romanbaranov <baranov.rp@gmail.com>
2018-07-04 21:04:01 +0200

    JOB4J-4745 Реализовать пользовательские фильтры для площадок машин [#4745]

--- pre-middle/chapter_010/salesplatform/frontend/src/components/PostList.vue
+++ pre-middle/chapter_010/salesplatform/frontend/src/components/PostList.vue
@@ -1,8 +1,15 @@
 <template>
   <b-container fluid>
+    <b-row class="main">
+      <b-form-select id="gaugeId"
+                     :options="bunch"
+                     v-model="visibility"
+                     @change="onChange($event)"
+      />
+    </b-row>
     <b-row class="main" v-show="posts && posts.length" v-cloak>
       <b-list-group class="post-list">
-        <job4j-post v-for="post in posts" :key="post.id"
+        <job4j-post v-for="post in filteredPosts" :key="post.id"
                     :post="post"></job4j-post>
       </b-list-group>
     </b-row>
@@ -13,17 +20,29 @@
 import axios from 'axios'
 import Post from './Post.vue'
 
+import { ALL, POSTED_TODAY, POSTS_WITH_PICS } from '../constants.js'
+
 export default {
   props: ['post'],
   data () {
     return {
-      posts: []
+      posts: [],
+      bunch: [ALL, POSTED_TODAY, POSTS_WITH_PICS],
+      visibility: ALL
     }
   },
   components: {
     'job4j-post': Post
   },
   beforeCreate () {
+    axios.get('/metadata')
+      .then(res => {
+        console.log(res)
+        let manufactures = res.data.manufactures.map(m => m.name) || []
+        this.bunch = this.bunch.concat(manufactures.reverse())
+        console.log(this.bunch)
+      })
+      .catch(error => console.log(error))
     axios.get('/list')
       .then(res => {
         console.log(res)
@@ -33,6 +52,39 @@ export default {
         console.log(posts)
       })
       .catch(error => console.log(error))
+  },
+  computed: {
+    filteredPosts: function () {
+      let posts
+      const visibility = this.visibility.toLowerCase()
+
+      switch (visibility) {
+        case POSTED_TODAY.toLowerCase():
+          posts = this.posts.filter(p => {
+            const pDate = new Date(p.publishDate).toDateString()
+            const today = new Date().toDateString()
+            return pDate === today
+          })
+          break
+        case POSTS_WITH_PICS.toLowerCase():
+          posts = this.posts.filter(p => p.pictures.length !== 0)
+          break
+        case ALL.toLowerCase():
+          posts = this.posts
+          break
+        default:
+          posts = this.posts.filter(p =>
+            visibility === p.car.carModel.manufacture.name.toLowerCase())
+      }
+      console.log(posts)
+      return posts || []
+    }
+  },
+  methods: {
+    onChange (value) {
+      this.visibility = value
+      console.log(`visibility: ${this.visibility}`)
+    }
   }
 }
 </script>
