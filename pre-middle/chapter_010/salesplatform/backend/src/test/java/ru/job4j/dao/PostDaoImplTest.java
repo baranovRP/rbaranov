0ada63f7363b9fa9f49d3c5cb7329028e6701e76
romanbaranov <baranov.rp@gmail.com>
2018-07-11 20:55:03 +0200

    JOB4J-4746 Добавить интеграционные тесты для площадок машин. [#4746]

--- pre-middle/chapter_010/salesplatform/backend/src/test/java/ru/job4j/dao/PostDaoImplTest.java
+++ pre-middle/chapter_010/salesplatform/backend/src/test/java/ru/job4j/dao/PostDaoImplTest.java
@@ -1,61 +1,106 @@
 package ru.job4j.dao;
 
+import org.junit.Before;
 import org.junit.Test;
-import ru.job4j.PostRepository;
 import ru.job4j.dao.car.CarDaoImpl;
 import ru.job4j.model.Post;
+import ru.job4j.model.Role;
+import ru.job4j.model.User;
+import ru.job4j.model.car.Car;
+import ru.job4j.model.car.parts.*;
 
-import java.sql.Timestamp;
-import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 public class PostDaoImplTest {
 
+    private User user;
+    private Car car;
+    private Post post;
+
+    @Before
+    public void setUp() {
+        user = new User()
+            .setEmail("ivanov@email.com")
+            .setPassw("1234")
+            .setRole(new Role(1L, "USER"))
+            .setPhone("555-55-55");
+        user.setId(new UserDaoImpl().create(user));
+        car = new Car()
+            .setBody(new Body().setId(2L))
+            .setCategory(new Category().setId(2L))
+            .setFuel(new Fuel().setId(2L))
+            .setCarModel(new CarModel().setId(2L))
+            .setTransmission(new Transmission().setId(2L))
+            .setEngineSize(1.2)
+            .setMileage(4000L)
+            .setYear(2018);
+        car.setId(new CarDaoImpl().create(car));
+        car = new CarDaoImpl().findOne(car.getId());
+        post = new Post()
+            .setContent("description")
+            .setPrice(35000D)
+            .setIsActive(Boolean.TRUE)
+            .setCar(car)
+            .setUser(user);
+        post.setId(new PostDaoImpl().create(post));
+        post = new PostDaoImpl().findOne(post.getId());
+    }
+
     @Test
     public void findOne() {
-        Post post = new PostDaoImpl().findOne(1L);
-        assertThat(post.getCar().getFuel().getType(), is("DIESEL"));
+        Post existingPost = new PostDaoImpl().findOne(post.getId());
+        assertThat(existingPost.getCar().getFuel().getType(), is("DIESEL"));
+        assertThat(existingPost.getUser().getEmail(), is("ivanov@email.com"));
     }
 
     @Test
     public void findAll() {
-        assertThat(new PostDaoImpl().findAll().size(), greaterThanOrEqualTo(1));
-    }
-
-    @Test
-    public void findWithoutPics() {
-        assertThat(new PostRepository().findWithPics().size(), greaterThan(3));
-    }
-
-    @Test
-    public void findTodaysPosts() {
-        assertThat(new PostRepository().findTodaysPosts().size(), equalTo(2));
-    }
-
-    @Test
-    public void findByCarManufacture() {
-        assertThat(new PostRepository().findByCarManufacture("BMW").size(), equalTo(3));
+        List<Post> posts = new PostDaoImpl().findAll();
+        List<String> emails = posts.stream()
+            .map(Post::getUser)
+            .map(User::getEmail)
+            .collect(Collectors.toList());
+        assertThat(posts.size(), greaterThanOrEqualTo(1));
+        assertThat("ivanov@email.com", isIn(emails));
     }
 
     @Test
     public void create() {
-        Post post = new Post().setContent("content")
-            .setPrice(5000.0)
-            .setIsActive(true)
-            .setCar(new CarDaoImpl().findOne(1L))
-            .setUser(new UserDaoImpl().findOne(1L))
-            .setPublishDate(new Timestamp(System.currentTimeMillis()))
-            .setPictures(new ArrayList<>());
-        Long id = new PostDaoImpl().create(post);
-        post.setId(id);
-        Post savedPost = new PostDaoImpl().findOne(id);
-        assertThat(savedPost, is(post));
+        Post newPost = new Post()
+            .setContent("steep wheelbarrow")
+            .setPrice(17000D)
+            .setIsActive(Boolean.FALSE)
+            .setCar(car)
+            .setUser(user);
+        newPost.setId(new PostDaoImpl().create(newPost));
+        newPost = new PostDaoImpl().findOne(newPost.getId());
+        Post savedPost = new PostDaoImpl().findOne(newPost.getId());
+        assertThat(savedPost, is(newPost));
+        assertThat(savedPost.getCar().getFuel().getType(), is("DIESEL"));
+        assertThat(savedPost.getUser().getEmail(), is("ivanov@email.com"));
+        assertThat(savedPost.getContent(), is("steep wheelbarrow"));
     }
 
     @Test
     public void update() {
+        Post newPost = new Post()
+            .setContent("steep wheelbarrow")
+            .setPrice(17000D)
+            .setIsActive(Boolean.FALSE)
+            .setCar(car)
+            .setUser(user);
+        newPost.setId(new PostDaoImpl().create(newPost));
+        newPost = new PostDaoImpl().findOne(newPost.getId());
+        new PostDaoImpl().update(newPost.setPrice(3000D));
+        Post savedPost = new PostDaoImpl().findOne(newPost.getId());
+        assertThat(savedPost, is(newPost));
+        assertThat(savedPost.getCar().getFuel().getType(), is("DIESEL"));
+        assertThat(savedPost.getUser().getEmail(), is("ivanov@email.com"));
+        assertThat(savedPost.getPrice(), is(3000D));
     }
 
     public void updateAll() {
@@ -63,9 +108,31 @@ public class PostDaoImplTest {
 
     @Test
     public void delete() {
+        Post newPost = new Post()
+            .setContent("steep wheelbarrow")
+            .setPrice(17000D)
+            .setIsActive(Boolean.FALSE)
+            .setCar(car)
+            .setUser(user);
+        newPost.setId(new PostDaoImpl().create(newPost));
+        assertThat(newPost.getId(), notNullValue());
+        new PostDaoImpl().delete(newPost);
+        Post foundPost = new PostDaoImpl().findOne(newPost.getId());
+        assertThat(foundPost, nullValue());
     }
 
     @Test
     public void deleteById() {
+        Post newPost = new Post()
+            .setContent("steep wheelbarrow")
+            .setPrice(17000D)
+            .setIsActive(Boolean.FALSE)
+            .setCar(car)
+            .setUser(user);
+        newPost.setId(new PostDaoImpl().create(newPost));
+        assertThat(newPost.getId(), notNullValue());
+        new PostDaoImpl().deleteById(newPost.getId());
+        Post foundPost = new PostDaoImpl().findOne(newPost.getId());
+        assertThat(foundPost, nullValue());
     }
 }
--- pre-middle/chapter_010/salesplatform/backend/src/test/java/ru/job4j/dao/UserDaoImplTest.java
+++ pre-middle/chapter_010/salesplatform/backend/src/test/java/ru/job4j/dao/UserDaoImplTest.java
@@ -1,49 +1,119 @@
 package ru.job4j.dao;
 
+import org.junit.Before;
 import org.junit.Test;
+import ru.job4j.model.Role;
 import ru.job4j.model.User;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.greaterThanOrEqualTo;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 
 public class UserDaoImplTest {
 
+    private User user;
+
+    @Before
+    public void setUp() {
+        user = new User().setEmail("ivanov@email.com")
+            .setPassw("1234")
+            .setRole(new Role(1L, "USER"))
+            .setPhone("555-55-55");
+        user.setId(new UserDaoImpl().create(user));
+    }
+
     @Test
     public void findOne() {
-        User user = new UserDaoImpl().findOne(1L);
         assertThat(user.getRole().getType(), is("USER"));
+        assertThat(user.getEmail(), is("ivanov@email.com"));
     }
 
     @Test
     public void findAll() {
-        assertThat(new UserDaoImpl().findAll().size(), greaterThanOrEqualTo(1));
+        List<User> users = new UserDaoImpl().findAll();
+        assertThat(users.size(), greaterThanOrEqualTo(1));
+        assertThat(users, hasItem(user));
     }
 
     @Test
     public void create() {
-        User user = new User().setEmail("ivanov2@email.com")
-            .setPassw("1234")
-            .setRole(new RoleDaoImpl().findOne(1L))
-            .setPhone("555-55-55");
-        Long id = new UserDaoImpl().create(user);
-        user.setId(id);
+        User newUser = new User().setEmail("miller@email.com")
+            .setPassw("4321")
+            .setRole(new Role(2L, "GUEST"))
+            .setPhone("222-22-22");
+        Long id = new UserDaoImpl().create(newUser);
+        newUser.setId(id);
         User savedUser = new UserDaoImpl().findOne(id);
-        assertThat(savedUser, is(user));
+        assertThat(savedUser, is(newUser));
+        assertThat(savedUser.getRole().getType(), is("GUEST"));
+        assertThat(savedUser.getEmail(), is("miller@email.com"));
     }
 
     @Test
     public void update() {
+        User newUser = new User().setEmail("miller@email.com")
+            .setPassw("4321")
+            .setRole(new Role(1L, "USER"))
+            .setPhone("222-22-22");
+        Long id = new UserDaoImpl().create(newUser);
+        newUser.setId(id);
+        new UserDaoImpl().update(newUser.setEmail("moreau@email.com"));
+        User savedUser = new UserDaoImpl().findOne(id);
+        assertThat(savedUser, is(newUser));
+        assertThat(savedUser.getRole().getType(), is("USER"));
+        assertThat(savedUser.getEmail(), is("moreau@email.com"));
     }
 
+    @Test
     public void updateAll() {
+        User newUser1 = new User().setEmail("fernandez@email.com")
+            .setPassw("4321")
+            .setRole(new Role(1L, "USER"))
+            .setPhone("222-22-22");
+        newUser1.setId(new UserDaoImpl().create(newUser1));
+        User newUser2 = new User().setEmail("lopez@email.com")
+            .setPassw("4321")
+            .setRole(new Role(1L, "USER"))
+            .setPhone("222-22-22");
+        newUser2.setId(new UserDaoImpl().create(newUser2));
+        List<User> changedUsers = Arrays.asList(newUser1.setEmail("rodriguez@email.com"),
+            newUser2.setRole(new Role(2L, "GUEST")));
+        new UserDaoImpl().updateAll(changedUsers);
+        assertThat(new UserDaoImpl().findOne(newUser1.getId()).getEmail(),
+            is("rodriguez@email.com"));
+        assertThat(new UserDaoImpl().findOne(newUser2.getId()).getEmail(),
+            is("lopez@email.com"));
+        assertThat(new UserDaoImpl().findOne(newUser1.getId()).getRole().getType(),
+            is("USER"));
+        assertThat(new UserDaoImpl().findOne(newUser2.getId()).getRole().getType(),
+            is("GUEST"));
     }
 
     @Test
     public void delete() {
+        User newUser = new User().setEmail("kowalski@email.com")
+            .setPassw("4321")
+            .setRole(new Role(1L, "USER"))
+            .setPhone("333-33-33");
+        Long id = new UserDaoImpl().create(newUser);
+        newUser.setId(id);
+        new UserDaoImpl().delete(newUser);
+        User foundUser = new UserDaoImpl().findOne(id);
+        assertThat(foundUser, nullValue());
     }
 
     @Test
     public void deleteById() {
+        User newUser = new User().setEmail("ruzicka@email.com")
+            .setPassw("4321")
+            .setRole(new Role(1L, "USER"))
+            .setPhone("333-33-33");
+        Long id = new UserDaoImpl().create(newUser);
+        newUser.setId(id);
+        new UserDaoImpl().deleteById(id);
+        User savedUser = new UserDaoImpl().findOne(id);
+        assertThat(savedUser, nullValue());
     }
 }
